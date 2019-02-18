c2c7a9d0e6c5034647f4edcd55fda361df1560f5
mazsi-on-xtrx <@>
2019-02-18 22:22:11 +0000

    add minimal non-project mode TCL build script

---
+++ nonproj.tcl
@@ -0,0 +1,26 @@
+#
+# based on 7 Series Integrated Block for PCIe v3.3, PG054, December 5, 2018
+#
+
+#read_verilog file.v
+read_vhdl top.vhd
+read_xdc system.xdc
+read_xdc bitconfig.xdc
+
+set_property TARGET_LANGUAGE VHDL [current_project]
+set_property PART xc7a35tcpg236-2 [current_project]
+
+#read_ip file.xci
+
+synth_design -top top
+
+opt_design
+place_design
+route_design
+
+report_timing_summary -max_paths 10 -file top_timing_summary_routed.rpt -warn_on_violation
+report_utilization -file top_utilization_placed.rpt
+report_io -file top_io_placed.rpt
+
+write_bitstream -force -bin_file top.bit
+
